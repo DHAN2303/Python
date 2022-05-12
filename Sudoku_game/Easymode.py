97e2e4eacaa56f22e524055b0909cf9dc823cefc
DHAN <75938963+DHAN2303@users.noreply.github.com>
2022-05-12 14:19:17 +0200

    tt

---
+++ Sudoku_game/Easymode.py
@@ -0,0 +1,191 @@
+from tkinter import *
+import random
+
+easysudoku = Tk()
+easysudoku.title('Easy Sudoku')
+
+sudoku = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+def valid(bord, row, col, num):
+    # pos_x = col
+    # pos_y = row
+    # num = number that we are trying to insert
+
+    # check row
+    if bord[row][col] != 0:
+        return False
+    else:
+        if num in bord[row]:
+            return False
+
+        # check col
+        for i in range(len(bord)):
+            if bord[i][col] == num:
+                return False
+
+        cube_x = col // 3  # Integer-Division (rundet): da kommt dann entweder 0, 1 oder 2 für die Spalte!
+        cube_y = row // 3  # Integer-Division (rundet): da kommt dann entweder 0, 1 oder 2 für die Reihe!
+
+        for i in range(cube_y * 3, cube_y * 3 + 3):
+            for j in range(cube_x * 3, cube_x * 3 + 3):
+                if bord[i][j] == num:
+                    return False
+
+        return True
+
+
+def find(bord):
+    for i in range(len(bord)):  # Länge der Reihe
+        for j in range(len(bord[0])):  # Länge der Spalte
+            if bord[i][j] == 0:
+                return (i, j)
+
+
+def solve(bord):
+    empty = find(bord=bord)
+
+    if not empty:
+        return True  # dann sind wir fertig!
+    else:
+        row, col = empty
+
+    for num in range(1, 10):  # 1-9
+        # position = str(row) +  "-" + str(col)
+        # lb = easysudoku.nametowidget(position)
+
+        if valid(bord=bord, row=row, col=col, num=num):
+            bord[row][col] = num
+            # lb.after(0, lambda: lb.config(bg='green', text=str(num)))
+
+            if solve(bord):  # BACKTRACKING - recursively (RÜCKWÄRTS!)
+                return True
+            else:
+                # lb.after(0, lambda: lb.config(bg='red', text=str(num)))
+                bord[row][col] = 0  # reset
+
+    # time.sleep(0.0001)
+
+    return False
+
+
+def print_board(board):
+    for i in board:
+        print(i)
+
+
+def placeBoard():
+    y = 0
+
+    for i in range(len(sudoku)):
+        x = 0
+
+        if i % 3 == 0 and i != 0:
+            display_lab = Label(easysudoku, text="  ", bg='gainsboro')
+            display_lab.grid(row=y, column=0)
+            y += 1
+
+        for j in range(len(sudoku[i])):
+
+            if j % 3 == 0 and j != 0:
+                display_lab = Label(easysudoku, text="  ", bg='gainsboro')  # heller Abstand zwischen den 9x9 Feldern
+                display_lab.grid(row=0, column=x)
+                x += 1
+            if j == 10:
+                x = 10
+            name = str(i) + '-' + str(j)
+
+            num = sudoku[i][j]
+            if num == 0:
+
+                display_lab = Label(easysudoku, text="  ", bg='grey', name=name)  # for row and col
+
+            else:
+                display_lab = Label(easysudoku, text=num, bg='white', name=name)  # for row and col
+
+            display_lab.grid(row=y, column=x)
+            # display_lab.config(bg='grey')
+
+            x += 1
+        y += 1
+
+
+# lb = Label(easysudoku, text='hello guys', name='guys')
+# lb.grid(row=10, column=10)
+
+
+# easysudoku.nametowidget(".0-9")
+
+def generate_sudoku():
+    for i in range(len(sudoku)):
+        for j in range(len(sudoku[i])):
+            sudoku[i][j] = 0
+
+    label_number = easysudoku.nametowidget('number')
+    number = label_number['text']
+
+    numbers_generated = 0
+
+    while numbers_generated < number:  # weil es von 0 zu zählen beginnt!
+        num = random.randint(1, 9)
+        paste_row = random.randint(0, 8)  # i know thats not really professional or scalable
+        paste_col = random.randint(0, 8)
+        test = valid(bord=sudoku, row=paste_row, col=paste_col, num=num)
+        if test:
+            sudoku[paste_row][paste_col] = num
+            print( str(numbers_generated) + ': ' +str(num) + ' generated on ' + str(paste_col) + ':' + str(paste_row))
+            numbers_generated += 1
+        print(str(numbers_generated) + ' numbers generated')
+
+    placeBoard()
+    solve(bord=sudoku)
+
+    for i in sudoku:
+        print(i)
+    print('')
+
+
+def plusNumber():
+    label_number = easysudoku.nametowidget('number')
+    text = label_number['text']
+    if text < 15:
+        label_number.config(text=text + 1)
+
+
+def minusNumber():
+    label_number = easysudoku.nametowidget('number')
+    text = label_number['text']
+    if text > 1:
+        label_number.config(text=text - 1)
+
+
+def blink():  # test function for changes with button
+    lb.config(bg='green')
+    lb.after(3000, lambda: lb.config(bg='white'))
+    
+placeBoard()
+position = "0-8"
+lb = easysudoku.nametowidget(position)
+
+b2 = Button(easysudoku, text="+", command=plusNumber)
+b3 = Button(easysudoku, text="-", command=minusNumber)
+b2.grid(row=12, column=12)
+b3.grid(row=13, column=12)
+
+lb = Label(easysudoku, text=8, bg='grey', name="number")
+lb.grid(row=12, column=13)
+
+b4 = Button(easysudoku, text="Generate", command=generate_sudoku, bg='yellow')
+b4.grid(row=12, column=14)
+
+b = Button(easysudoku, text="Solve!", command=placeBoard, bg='green')
+b.grid(row=13, column=14)
+
+easysudoku.mainloop()
---
+++ Sudoku_game/sudokumain.py
@@ -0,0 +1,68 @@
+from tkinter import *
+from tkinter import ttk
+
+
+
+def save_player_info():
+    player_info = playerName.get()
+
+    if player_info == '':
+        return Label(sudokumain,text='Please Write Your Name',width=50,fg='firebrick2',font=("Arial", 15, "bold")).grid(row=6, column=0,columnspan=2)
+
+    file = open("user.txt", "a")
+    file.write(player_info)
+    file.write("\n")
+    file.close()
+
+def exit():
+    return
+
+def leaderboard1():
+    # leaderboard
+
+    Label(sudokumain, text='★★★★★★LEADERBOARD★★★★★★\nTHE BEST TIME WASTER IN THE WORLD',
+                             bg="#EEC900", width=100, fg='black', font=("Arial", 15, "bold")).grid(row=1, column=0,
+                                                                                                  columnspan=2)
+
+    leaderboard = ttk.Treeview(sudokumain, column=("c1", "c2", "c3","c4"), show='headings', height=10)
+    leaderboard.grid(row=2, column=0, columnspan=2)
+    leaderboard.column("# 1", anchor=CENTER,width=225,)
+    leaderboard.heading("# 1", text="ID")
+    leaderboard.column("# 2", anchor=CENTER,width=225,)
+    leaderboard.heading("# 2", text="Player Name")
+    leaderboard.column("# 3", anchor=CENTER,width=225,)
+    leaderboard.heading("# 3", text="Score")
+    leaderboard.column("# 4", anchor=CENTER, width=225, )
+    leaderboard.heading("# 4", text="Time")
+
+    f = open("user.txt", "r")
+    i = 0
+    for x in f:
+        i = i + 1
+        leaderboard.insert('', END, text="1", values=(i, x, ''))
+    f.close()
+
+    # number of players
+    Label(text=f"Number of Players: {i}", bg="#EEC900", width=100, fg='black',
+                    font=("Arial", 15, "bold")).grid(row=3, column=0, columnspan=2)
+
+
+sudokumain = Tk()
+sudokumain.title('CHINESE SUDOKU')
+# welcome
+wellcomelabel = Label(text='WELLCOME TO THE CHEAPEST SUDOKU GAME IN THE WORLD', fg="black", bg="yellow",width=100, height=3, font=10).grid(row=0, column=0, columnspan=10)
+
+leaderboard1()
+
+# PLAYER NAME
+playerNamel = Label(sudokumain,text="Your Name :",font=("Helvetica", 15),pady=20).grid(row=4, column=0,columnspan=1)
+playerName = StringVar()
+playerName_entry = Entry(sudokumain,textvariable=playerName, width=50,justify='center',bd =3,fg='red' ).grid(row=4, column=1,columnspan=2)
+
+# buttons
+STARTbutton = Button(sudokumain, text="Let's Play", command=save_player_info, width=20, height="1",activeforeground='green',font=("Helvetica", 20, "bold"),pady=5)
+STARTbutton.grid(row=5, column=0,columnspan=1)
+Exitbutton = Button(sudokumain, text="EXIT",command=lambda :sudokumain.quit(), width=20, height="1",activeforeground='red',font=("Helvetica", 20, "bold"),pady=5)
+Exitbutton.grid(row=5, column=1,columnspan=2)
+
+sudokumain.mainloop()
